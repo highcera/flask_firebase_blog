544a8a29f4b5340e95cef794edeab93efad6ee80
highcera <highcera@gmail.com>
2022-11-13 19:58:55 +0900

    tailwind 전까지

--- DB_handler.py
+++ DB_handler.py
@@ -1,27 +1,66 @@
 import pyrebase
 import json
+import uuid
 
 class DBModule:
 	def __init__(self):
 		with open("./auth.json") as f:
 			config = json.load(f)
 
-		self.firebase = pyrebase.initialize_app(config)
+		firebase = pyrebase.initialize_app(config)
+		self.db = firebase.database()
 
-	def login(self, id, pwd):
-		pass
+	def login(self, uid, pwd):
+		users = self.db.child("users").get().val()
+		try:
+			userinfo = users[uid]
+			if userinfo["pwd"] == pwd:
+				print("가입되어 있습니다.")
+				return True
+			else:
+				print("비밀번호를 잘못 입력했습니다.")
+				return False
+		except:
+			print("등록되지 않은 아이디입니다.")
+			return False	
 
-	def signin(self, id, pwd, name, email):
-		pass
+	def signin_verification(self, uid):
+		users = self.db.child("users").get().val()
+		for i in users:
+			if uid == i:
+				return False
+		return True	
 
-	def write_post(self, user, contents):
-		pass
+	def signin(self, _id_, pwd, name, email):
+		# print(_id_, pwd, name, email)
+		information = {
+        	"pwd": pwd,
+			"name": name,
+			"email": email
+		}
+		if self.signin_verification(_id_):
+			self.db.child("users").child(_id_).set(information)
+			return True
+		else:
+			return False
+
+	def write_post(self, title, contents, uid):
+		pid = str(uuid.uuid4())[:10]
+		print(pid)
+		information = {
+		"title": title,
+		"contents": contents,
+		"uid": uid
+		}
+		self.db.child("posts").child(pid).set(information)
 
 	def post_list(self):
-		pass
+		post_lists = self.db.child("posts").get().val()
+		return post_lists
 
 	def post_detail(self, pid):
-		pass
+		post = self.db.child("posts").get().val()[pid]
+		return post
 
 	def get_user(self, uid):
 		pass
--- app.py
+++ app.py
@@ -1,47 +1,100 @@
-from flask import Flask, redirect, render_template, url_for, request 
+from flask import Flask, redirect, render_template, url_for, request, flash, session
 from DB_handler import DBModule
 
 app = Flask(__name__)
+app.secret_key = "asdjkflgllsdkkfj"
 DB = DBModule()
 
 @app.route("/")
 def index(): 
-    return render_template("index.html")
+    if "uid" in session:
+        user = session["uid"]
+    else:
+        user = "Login"   
+    return render_template("index.html", user = user)
 
 @app.route("/list")
 def post_list(): 
-    pass
+    post_list = DB.post_list()
 
-# @app.route("/post/<int: pid>") 
-# def post (pid): 
-#     pass
+    if post_list == None:
+        length = 0
+    else:
+        length = len(post_list)
+    return render_template("post_list.html", post_list = post_list.items(), length = length)
 
+@app.route("/post/<string:pid>") 
+def post(pid): 
+    post = DB.post_detail(pid)
+    print(post)
+    return render_template("post_detail.html", post = post)
+ 
+@app.route("/logout") 
+def logout(): 
+    if "uid" in session:
+        session.pop("uid")
+        return redirect(url_for("index"))
+    else:
+        return redirect(url_for("login"))
+   
 @app.route("/login") 
 def login(): 
-    pass
+    if "uid" in session:
+        return redirect(url_for("index"))
+    return render_template("login.html")
 
-@app.route("/login_done") 
+@app.route("/login_done", methods = {"GET"}) 
 def login_done(): 
-    pass
+    if "uid" in session:
+        return redirect(url_for("index"))
+    uid = request.args.get("uid")
+    pwd = request.args.get("pwd")
+    print(uid, pwd)
+
+    if DB.login(uid, pwd): 
+        session["uid"] = uid
+        flash("인증되었습니다.")
+        return redirect(url_for("index"))
+    else:
+        flash("아이디가 없거나 비밀번호가 틀립니다.")
+        return redirect(url_for("login"))
 
 @app.route("/signin") 
 def signin(): 
     return render_template("signin.html")
 
-@app.route("/signin_done") 
+@app.route("/signin_done", methods = {"GET"}) 
 def signin_done(): 
-    pass
+    email = request.args.get("email")
+    uid = request.args.get("uid")
+    pwd = request.args.get("pwd")
+    name = request.args.get("name")
+    # print(email, uid, pwd, name)
+    if DB.signin(_id_ = uid, pwd = pwd, name = name, email = email): 
+        # flash("정상적으로 등록되었습니다.")
+        return redirect(url_for("index"))
+    else:
+        flash("이미 존재하는 아이디입니다.")
+        return redirect(url_for("signin"))
+
 
 @app.route("/write") 
 def write(): 
-    pass
+    if "uid" in session:
+        return render_template("write_post.html")
+    else:
+        return redirect(url_for("login"))
+
 
 @app.route("/write_done", methods = {"GET"}) 
 def write_done(): 
-    pass
+    title = request.args.get("title")
+    contents = request.args.get("contents")
+    uid = session.get("uid")
+    print (title, contents, uid)
+    DB.write_post(title, contents, uid)
+
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
-
-# signin = {"password" : "dafslkjla30", "username":"js"}
-# db.child("users").child("abcd").set(signin)
+    app.run(debug=True)
